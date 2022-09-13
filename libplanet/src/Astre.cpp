e6dfe4d82e69d832dbbcb5f0c468baf6307dcc16
gtwice <g.defoy@technologyandstrategy.com>
2022-09-13 17:39:54 +0200

    [Guidelines] + file extensions + namespaces + include guards + format

--- libplanet/src/Astre.cpp
+++ libplanet/src/Astre.cpp
@@ -1,95 +1,72 @@
-#include "Astre.h"
-#include "Point.h"
-#include "Sphere.h"
+#include "Astre.hpp"
 
 #include <iostream>
 
-namespace libPlanet {
+#include "Point.hpp"
+#include "Sphere.hpp"
+
+namespace planets {
 
 void Astre::printInit() {
-    auto pos = getPosition();
-    double x = pos._x;
-    double y = pos._y;
-    double z = pos._z;
-    std::cout << "Created an Astre of mass " << getMass() << ", and diameter "
-              << getDiameter() << ", at (" << x << ", " << y << ", " << z << ")"
-              << std::endl;
+  auto pos = getPosition();
+  double x = pos._x;
+  double y = pos._y;
+  double z = pos._z;
+  std::cout << "Created an Astre of mass " << getMass() << ", and diameter "
+            << getDiameter() << ", at (" << x << ", " << y << ", " << z << ")"
+            << std::endl;
 }
 
 Astre::Astre(Sphere s, double density) : Sphere(s), _density(density) {
-    printInit();
+  printInit();
 }
 
 Astre::Astre(Point point, double diameter, double density)
     : Sphere(point, diameter), _density(density) {
-    printInit();
+  printInit();
 }
 
 Astre::~Astre() = default;
 
-double Astre::getMass() const {
-    return getVolume() * _density;
-}
+double Astre::getMass() const { return getVolume() * _density; }
 
-double Astre::getDensity() const {
-    return _density;
-}
+double Astre::getDensity() const { return _density; }
 
 /*
 {F}_{{A/B}}={F}_{{B/A}}=G{\frac {M_{A}M_{B}}{d^{2}}}
 */
 double Astre::getAttraction(Astre& b) {
-    const double G = 6.67430e-11;
-    const double d = getDistance_center(b);
-    return (G * getMass() * b.getMass()) / (d * d);
+  const double G = 6.67430e-11;
+  const double d = getDistance_center(b);
+  return (G * getMass() * b.getMass()) / (d * d);
 }
 
 double Astre::sumAttraction(std::vector<Astre*> list) {
-    double sum = 0;
-    for (auto astre : list) {
-        sum += this->getAttraction(*astre);
-    }
-    return sum;
+  double sum = 0;
+  for (auto astre : list) {
+    sum += this->getAttraction(*astre);
+  }
+  return sum;
 }
 
-bool Astre::operator==(Sphere& rhs) {
-    return rhs == *this;
-}
+bool Astre::operator==(Sphere& rhs) { return rhs == *this; }
 
-bool Astre::operator<=(Sphere& rhs) {
-    return rhs >= *this;
-}
+bool Astre::operator<=(Sphere& rhs) { return rhs >= *this; }
 
-bool Astre::operator>=(Sphere& rhs) {
-    return rhs <= *this;
-}
+bool Astre::operator>=(Sphere& rhs) { return rhs <= *this; }
 
-bool Astre::operator>(Sphere& rhs) {
-    return rhs < *this;
-}
+bool Astre::operator>(Sphere& rhs) { return rhs < *this; }
 
-bool Astre::operator<(Sphere& rhs) {
-    return rhs > *this;
-}
+bool Astre::operator<(Sphere& rhs) { return rhs > *this; }
 
-bool Astre::operator==(Astre& rhs) {
-    return getMass() == rhs.getMass();
-}
+bool Astre::operator==(Astre& rhs) { return getMass() == rhs.getMass(); }
 
-bool Astre::operator<=(Astre& rhs) {
-    return getMass() <= rhs.getMass();
-}
+bool Astre::operator<=(Astre& rhs) { return getMass() <= rhs.getMass(); }
 
-bool Astre::operator>=(Astre& rhs) {
-    return getMass() >= rhs.getMass();
-}
+bool Astre::operator>=(Astre& rhs) { return getMass() >= rhs.getMass(); }
 
-bool Astre::operator>(Astre& rhs) {
-    return getMass() > rhs.getMass();
-}
+bool Astre::operator>(Astre& rhs) { return getMass() > rhs.getMass(); }
 
-bool Astre::operator<(Astre& rhs) {
-    return getMass() < rhs.getMass();
-}
+bool Astre::operator<(Astre& rhs) { return getMass() < rhs.getMass(); }
 
-}  // namespace libPlanet
+}  // namespace planets
--- libplanet/src/Sphere.cpp
+++ libplanet/src/Sphere.cpp
@@ -1,70 +1,68 @@
-#include "Sphere.h"
+#include "Sphere.hpp"
 
-#include <math.h>
 #include <cmath>
 #include <string>
 
-#include "Point.h"
-#include "Position.h"
-namespace libPlanet {
+#include "Point.hpp"
+#include "Position.hpp"
+
+namespace planets {
 
 void Sphere::printInit() {
-    std::cout << "Created a new Sphere at " << *this
-              << " of volume : " << getVolume() << "\n";
+  std::cout << "Created a new Sphere at " << *this
+            << " of volume : " << getVolume() << "\n";
 }
 
 Sphere::Sphere(Point p, double diameter)
     : Point(p.getName(), p.getPosition()), _diameter(diameter) {
-    printInit();
+  printInit();
 }
 
 Sphere::Sphere(Sphere& s)
     : Point(s.getName(), s.getPosition()), _diameter(s.getDiameter()) {
-    printInit();
+  printInit();
 }
 
 Sphere::~Sphere() = default;
 
 double Sphere::getVolume() const {
-    return 4 * M_PI * pow(_diameter / 2, 3) / 3;
+  return 4 * M_PI * pow(_diameter / 2, 3) / 3;
 }
 
 double Sphere::getDistance_center(Point& rhs) const {
-    return Point::getDistance(rhs);
+  return Point::getDistance(rhs);
 }
 
 double Sphere::getDistance(Sphere& s) const {
-    double rawDist = Point::getDistance(s);
-    return rawDist - (s._diameter + _diameter) / 2;
+  double rawDist = Point::getDistance(s);
+  return rawDist - (s._diameter + _diameter) / 2;
 }
 
 static bool almostEquals(double a, double b) {
-    const double epsilon = 0.00001;
-    return std::abs(a - b) < epsilon;
+  const double epsilon = 0.00001;
+  return std::abs(a - b) < epsilon;
 }
 
 bool Sphere::operator==(Sphere& rhs) const {
-    return almostEquals(_diameter, rhs._diameter);
+  return almostEquals(_diameter, rhs._diameter);
 }
 
 bool Sphere::operator<=(Sphere& rhs) const {
-    return _diameter <= rhs._diameter;
+  return _diameter <= rhs._diameter;
 }
 
 bool Sphere::operator>=(Sphere& rhs) const {
-    return _diameter >= rhs._diameter;
+  return _diameter >= rhs._diameter;
 }
 
 bool Sphere::operator>(Sphere& rhs) const {
-    return _diameter > rhs._diameter;
+  return _diameter > rhs._diameter;
 }
 
 bool Sphere::operator<(Sphere& rhs) const {
-    return _diameter < rhs._diameter;
+  return _diameter < rhs._diameter;
 }
 
-double Sphere::getDiameter() const {
-    return _diameter;
-}
+double Sphere::getDiameter() const { return _diameter; }
 
-}  // namespace libPlanet
+}  // namespace planets
