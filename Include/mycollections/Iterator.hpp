7636c96f3f9c02b195e3ced7c3564206d5f43487
gtwice <g.defoy@technologyandstrategy.com>
2022-10-10 16:09:25 +0200

    improved iterator

--- Include/mycollections/Iterator.hpp
+++ Include/mycollections/Iterator.hpp
@@ -1,5 +1,5 @@
-#ifndef SRC_INCLUDE_MYCOLLECTIONS_ITERATOR
-#define SRC_INCLUDE_MYCOLLECTIONS_ITERATOR
+#ifndef INCLUDE_MYCOLLECTIONS_ITERATOR
+#define INCLUDE_MYCOLLECTIONS_ITERATOR
 
 namespace mycollections {
 
@@ -16,7 +16,7 @@ public:
    * @param first the first element of the container
    * @param end the pointer of the end of the container
    */
-  Iterator(pointer first, pointer end) : _first(first), _current(first), _end(end){};
+  Iterator(pointer first, pointer end) : _first(first), _current(first), _end(end), _index(0){};
 
   /**
    * @brief Increases the pointer of the iterator by one and returns the current value
@@ -26,11 +26,12 @@ public:
   value_type next() {
     value_type ret = *_current;
     _current++;
+    _index++;
     return ret;
   };
 
   bool hasNext() const {
-    return _end != _current;
+    return _end != _current && _index < _max;
   };
 
   /**
@@ -47,11 +48,47 @@ public:
     return c;
   }
 
+  /**
+   * @brief Calls a lambda on each elements of the iterator
+   *
+   * Exemple to print all the elements of the iterator
+   * @code {.cpp}
+   * auto iter = mycollections::StaticVector<...,...> myVec;
+   * myVec.iter().forEach([](auto v) { std::cout << *v << "\n"; });
+   * @endcode
+   * 
+   * @tparam Functor a C++ lambda
+   * @param f the lambda to call
+   * @return Iterator& the iterator that has been reset to allow to chain calls to Iterator methods
+   */
   template <typename Functor>
   Iterator& forEach(Functor f) {
     while (hasNext()) {
       f(next());
     }
+
+    return reset();
+  }
+
+  Iterator& skip(unsigned int n) {
+    for (unsigned int i = 0; i < n && hasNext(); i++) {
+      next();
+      _index--;
+    }
+    return *this;
+  }
+
+  Iterator& take(unsigned int n) {
+    _max = n;
+    return *this;
+  }
+
+  /**
+   * @brief Resets the iterator, making the current element point to the first element
+   * 
+   * @return Iterator& 
+   */
+  Iterator& reset() {
     _current = _first;
     return *this;
   }
@@ -83,7 +120,9 @@ private:
   const pointer _first;
   pointer _current;
   const pointer _end;
+  unsigned int _index;
+  unsigned int _max = -1;
 };
 
 }  // namespace mycollections
-#endif /* SRC_INCLUDE_MYCOLLECTIONS_ITERATOR */
+#endif /* INCLUDE_MYCOLLECTIONS_ITERATOR */
