1c615532cbd90f1e01c78b768952751bde6de561
gtwice <g.defoy@technologyandstrategy.com>
2022-10-14 11:03:34 +0200

    added asteroids

---
+++ Include/planets/Asteroid.hpp
@@ -0,0 +1,35 @@
+#ifndef INCLUDE_PLANETS_ASTEROID
+#define INCLUDE_PLANETS_ASTEROID
+
+#include <utility>
+#include "Astre.hpp"
+#include "Planet.hpp"
+#include "Vec3.hpp"
+
+namespace planets {
+class Asteroid : public Planet {
+public:
+  Asteroid(Asteroid&&) = default;
+  ~Asteroid() = default;
+  Asteroid(Planet&& rhs);
+  Asteroid(Planet&&, Vec3 vel);
+
+  /**
+   * @brief Creates an Asteroid with random coordinates and velocity
+   * 
+   * @param id the number to add at the end of the name of the asteroid
+   * @return Asteroid 
+   */
+  static Asteroid withRandomCoordinates(int id);
+
+  /**
+   * @brief Checks for collisions with the given planets
+   * 
+   * @param it the collection's iterator to check for collisions 
+   */
+  void checkCollisions(mycollections::Iterator<Planet*> it) override;
+};
+
+}  // namespace planets
+
+#endif /* INCLUDE_PLANETS_ASTEROID */
---
+++ Source/planets/Asteroid.cpp
@@ -0,0 +1,40 @@
+#include "Asteroid.hpp"
+#include <random>
+#include <string>
+
+#include "Astre.hpp"
+#include "Planet.hpp"
+#include "Point.hpp"
+#include "Position.hpp"
+#include "Sphere.hpp"
+#include "Vec3.hpp"
+
+namespace planets {
+
+Asteroid::Asteroid(Planet&& rhs, Vec3 vel) : Planet(std::move(rhs)) {
+  setVelocity(vel);
+}
+Asteroid::Asteroid(Planet&& rhs) : Planet(std::move(rhs)){};
+
+Asteroid Asteroid::withRandomCoordinates(int id) {
+  std::random_device rd;
+  std::uniform_real_distribution<double> distVel(-100, 100);
+  std::uniform_real_distribution<double> distPos(-50000, 50000);
+
+  auto vel = Vec3(distVel(rd), distVel(rd), distVel(rd));
+  auto position = Position(distPos(rd), distPos(rd), distPos(rd));
+  //   return Asteroid(Planet(Astre(Sphere(Point("Asteroid_" + std::to_string(id), position), 10), 0), vel));
+  return Asteroid(Planet(Astre(Sphere(Point("A_" + std::to_string(id), position), 0), 0), vel));
+}
+
+void Asteroid::checkCollisions(mycollections::Iterator<Planet*> it) {
+  while (it.hasNext()) {
+    auto planet = it.next();
+    if (this != planet && doesItCollide(*planet)) {
+      observers.iter().forEach([this, &planet](cli::Observer* obs) { obs->update("The planet " + getName() + "has collided with the Asteroid [" + planet->getName() + "]"); });
+      _crashed = true;
+    }
+  }
+}
+
+}  // namespace planets
